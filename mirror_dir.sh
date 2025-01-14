4f06cd56ab7ceaba26d8c59ac27b5e42cb0e0bec
Andreas Loehr <andreas.loehr@simplan.de>
2025-01-14 15:31:48 +0100

    Tue Jan 14 15:31:48 CET 2025 updating softlinks.

--- mirror_dir.sh
+++ mirror_dir.sh
@@ -6,7 +6,13 @@ find . -type l -delete
 echo $(date)
 
 ln -s /mnt/c/Users/andreas.loehr/Documents/notes/*.org .
+
+
+up_files = $(git ls-files --modified)
+echo ${up_files}
+
 git add "*.org"
+
 git add "*.sh"
 git commit -m "$(date) updating softlinks."
 git push
