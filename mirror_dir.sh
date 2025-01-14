cd48c693d06fc9f36b2427cc6614983de80b0efd
Andreas Loehr <andreas.loehr@simplan.de>
2025-01-14 15:39:05 +0100

    Tue Jan 14 15:39:05 CET 2025 updating softlinks of  and .

--- mirror_dir.sh
+++ mirror_dir.sh
@@ -9,11 +9,11 @@ ln -s /mnt/c/Users/andreas.loehr/Documents/notes/*.org .
 
 
 up_files = $(git ls-files --modified)
-echo $up_files
+mod_files = $(git ls-files --others)
+echo "$upfiles $mod_files"
 
 git add "*.org"
-
 git add "*.sh"
-git commit -m "$(date) updating softlinks."
-git push
+git commit -m "$(date) updating softlinks of $up_files and $mod_files."
+git pusho
 echo "Completed softlink update."
