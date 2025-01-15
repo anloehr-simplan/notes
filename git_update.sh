52398a83c44563f87ff4135f44f3e03e704e4f20
Andreas Loehr <andreas.loehr@simplan.de>
2025-01-15 18:44:49 +0100

    Wed Jan 15 18:44:49 CET 2025 updating softlinks of mirror_dir.sh and ImprovingTextEmbeddingsWithLLMs.org git_update.sh model_checkpointing.org.

--- git_update.sh
+++ git_update.sh
@@ -1,13 +1,3 @@
-#! /bin/bash
-
-# delete existing softlinks
-find . -type l -delete
-
-echo $(date)
-
-ln /mnt/c/Users/andreas.loehr/Documents/notes/*.org .
-
-
 up_files="$(git ls-files --modified --exclude-standard)"
 mod_files="$(git ls-files --others --exclude-standard)"
 echo "$upfiles $mod_files"
